8780181cd3497b13e5a6fe0c112a3a90660cd973
TwistedTornado <144802073+TwistedTornado@users.noreply.github.com>
2023-09-16 18:39:43 +0100

    Create lexer.rs with Lexer struct

---
+++ src/lexer.rs
@@ -0,0 +1,19 @@
+use std::iter::{Enumerate, Peekable};
+
+pub struct Lexer<I>
+where
+    I: Iterator<Item = char>,
+{
+    source: Peekable<Enumerate<I>>,
+}
+
+impl<I> Lexer<I>
+where
+    I: Iterator<Item = char>,
+{
+    pub fn new(source: I) -> Self {
+        Self {
+            source: source.enumerate().peekable(),
+        }
+    }
+}
