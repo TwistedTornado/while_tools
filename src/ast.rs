eb9996c77995789f88ba08d0011101d7adb8ead8
TwistedTornado <144802073+TwistedTornado@users.noreply.github.com>
2023-09-17 16:01:44 +0100

    Add initial AST type in ast.rs

---
+++ src/ast.rs
@@ -0,0 +1,64 @@
+//! A module for the abstract syntax tree (AST) that many tools will use.
+
+/// A recursive type representing the AST. The AST itself stores no
+/// functionality or logic on how each node behaves -- that is the purview
+/// of other tools that use the AST, like the interpreter.
+#[derive(Debug, Clone)]
+pub enum Ast {
+    // Literals and identifiers
+    True,
+    False,
+    Literal(i32),
+    Ident(String),
+
+    // Comparison and equality
+    Not {
+        expr: Box<Self>,
+    },
+    Eq {
+        left: Box<Self>,
+        right: Box<Self>,
+    },
+    LessEq {
+        left: Box<Self>,
+        right: Box<Self>,
+    },
+    And {
+        left: Box<Self>,
+        right: Box<Self>,
+    },
+
+    // Arithmetic
+    Add {
+        left: Box<Self>,
+        right: Box<Self>,
+    },
+    Sub {
+        left: Box<Self>,
+        right: Box<Self>,
+    },
+    Mul {
+        left: Box<Self>,
+        right: Box<Self>,
+    },
+
+    // Statements
+    Comp {
+        first: Box<Self>,
+        second: Box<Self>,
+    },
+    Ass {
+        ident: String,
+        value: Box<Self>,
+    },
+    Skip,
+    If {
+        cond: Box<Self>,
+        true_path: Box<Self>,
+        false_path: Box<Self>,
+    },
+    While {
+        cond: Box<Self>,
+        body: Box<Self>,
+    },
+}
--- src/lib.rs
+++ src/lib.rs
@@ -3,4 +3,5 @@
 //! foundational concepts about computation and correctness. It is heavily
 //! inspired by a language described by C. A. Hoare in
 //! [An Axiomatic Basis for Computer Programming](https://dl.acm.org/doi/10.1145/363235.363259).
+pub mod ast;
 pub mod lexer;
