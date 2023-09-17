32f290a710cbffb034f07c0c3cf1241e68b2c138
TwistedTornado <144802073+TwistedTornado@users.noreply.github.com>
2023-09-17 14:49:13 +0100

    Add tests for ASI in lexer_test.rs

--- tests/lexer_test.rs
+++ tests/lexer_test.rs
@@ -53,3 +53,72 @@ fn test_parse_error_spans() {
         .as_ref()
         .is_err_and(|err| err.span == Span(6, 7)));
 }
+
+/// Test the automatic semicolon insertion (ASI) when there is no existing
+/// semicolon.
+#[test]
+fn test_asi_when_no_semicolon() {
+    let lexer = Lexer::new("skip\r\nskip".chars());
+    let tokens: Vec<_> = lexer.map(|result| result.unwrap().inner).collect();
+
+    assert_eq!(tokens, vec![Token::Skip, Token::Semicolon, Token::Skip]);
+}
+
+/// Test the automatic semicolon insertion (ASI) when there already is a
+/// semicolon between two lines.
+#[test]
+fn test_asi_when_already_semicolon() {
+    let lexer = Lexer::new("skip;\r\nskip".chars());
+    let tokens: Vec<_> = lexer.map(|result| result.unwrap().inner).collect();
+
+    assert_eq!(tokens, vec![Token::Skip, Token::Semicolon, Token::Skip]);
+}
+
+/// Test the automatic semicolon insertion (ASI) when the semicolon is on its
+/// own line between two other lines. This should not be condensed -- we should
+/// get two consecutive semicolons.
+#[test]
+fn test_asi_semicolon_on_own_line() {
+    let lexer = Lexer::new("skip\r\n;\r\nskip".chars());
+    let tokens: Vec<_> = lexer.map(|result| result.unwrap().inner).collect();
+
+    assert_eq!(
+        tokens,
+        vec![Token::Skip, Token::Semicolon, Token::Semicolon, Token::Skip]
+    );
+}
+
+/// Test the automatic semicolon insertion (ASI) when there are multiple
+/// line-breaks between two other lines. This should be condensed into one
+/// Semicolon token.
+#[test]
+fn test_asi_multiple_linebreaks() {
+    let lexer = Lexer::new("skip\r\n\r\n\r\nskip".chars());
+    let tokens: Vec<_> = lexer.map(|result| result.unwrap().inner).collect();
+
+    assert_eq!(tokens, vec![Token::Skip, Token::Semicolon, Token::Skip]);
+}
+
+/// Test the automatic semicolon insertion (ASI) when there are consecutive
+/// explicit semicolon characters. This should not be condensed -- we should
+/// have that same amount of Semicolon tokens.
+#[test]
+fn test_asi_multiple_semicolons() {
+    let lexer = Lexer::new("skip;;skip".chars());
+    let tokens: Vec<_> = lexer.map(|result| result.unwrap().inner).collect();
+
+    assert_eq!(
+        tokens,
+        vec![Token::Skip, Token::Semicolon, Token::Semicolon, Token::Skip]
+    );
+}
+
+/// Test the automatic semicolon insertion (ASI) when there are non-Windows
+/// line-endings such as simply LF (\n).
+#[test]
+fn test_asi_non_crlf_endings() {
+    let lexer = Lexer::new("skip\nskip".chars());
+    let tokens: Vec<_> = lexer.map(|result| result.unwrap().inner).collect();
+
+    assert_eq!(tokens, vec![Token::Skip, Token::Semicolon, Token::Skip]);
+}
