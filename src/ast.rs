0e36b8f38c5a2392f3bbafc5a3acad7be029eb9b
TwistedTornado <144802073+TwistedTornado@users.noreply.github.com>
2023-09-17 17:13:15 +0100

    Derive Eq and PartialEq for AST

--- src/ast.rs
+++ src/ast.rs
@@ -5,7 +5,7 @@ mod macros;
 /// A recursive type representing the AST. The AST itself stores no
 /// functionality or logic on how each node behaves -- that is the purview
 /// of other tools that use the AST, like the interpreter.
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Eq, PartialEq)]
 pub enum Ast {
     // Literals and identifiers
     True,
