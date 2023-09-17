9b58c155f7a6627502e57a91feb2d84e64ad86d2
TwistedTornado <144802073+TwistedTornado@users.noreply.github.com>
2023-09-17 17:15:07 +0100

    Add tests for binary_node! macro

--- tests/ast_test.rs
+++ tests/ast_test.rs
@@ -1,1 +1,26 @@
 use while_tools::ast::*;
+use while_tools::{binary_node, literal};
+
+/// Test that the generalised binary node macro works.
+#[test]
+fn test_ast_binary_node_macro() {
+    let add_node = binary_node!(Add, literal!(1), literal!(2));
+
+    assert_eq!(
+        add_node,
+        Ast::Add {
+            left: Box::new(Ast::Literal(1)),
+            right: Box::new(Ast::Literal(2))
+        }
+    );
+
+    let less_eq_node = binary_node!(LessEq, literal!(3), literal!(4));
+
+    assert_eq!(
+        less_eq_node,
+        Ast::LessEq {
+            left: Box::new(Ast::Literal(3)),
+            right: Box::new(Ast::Literal(4))
+        }
+    )
+}
