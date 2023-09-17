b1656b22eb7d9ddb2b1d034726567b89ee926302
TwistedTornado <144802073+TwistedTornado@users.noreply.github.com>
2023-09-17 14:49:43 +0100

    Add example showing what information Lexer returns for a token

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use while_tools::lexer::Lexer;
+
 fn main() {
-    println!("Hello, world!");
+    // A basic example showing what information the lexer may return for a
+    // token. Although each element contains a substantial amount of nesting,
+    // it encodes a lot of information and takes advantage of idiomatic
+    // Rust structures.
+    let tokens: Vec<_> = Lexer::new("skip".chars()).collect();
+    println!("{:#?}", tokens);
 }
