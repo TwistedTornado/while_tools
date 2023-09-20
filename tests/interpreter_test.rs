614c7a5e448cb87b266297c8792f7d179dd2972c
TwistedTornado <144802073+TwistedTornado@users.noreply.github.com>
2023-09-20 14:38:24 +0100

    Add test and framework for interpreter testing

---
+++ tests/interpreter_test.rs
@@ -0,0 +1,43 @@
+use while_tools::ast::Ast;
+use while_tools::interpreter::state::State;
+use while_tools::interpreter::Interpreter;
+use while_tools::lexer::Lexer;
+use while_tools::parser::*;
+use while_tools::{
+    ass_stmt, binary_node, comp_stmt, ident, if_stmt, less_eq, literal, literal_true, skip_stmt,
+    while_stmt,
+};
+
+/// Almost like a procedural macro to make the pipeline of running a program
+/// more DRY.
+fn get_program_result(source: &str) -> State {
+    let tokens = Lexer::new(source.chars());
+    let result: Vec<_> = match tokens.collect::<Result<Vec<_>, _>>() {
+        Ok(v) => v,
+        Err(e) => {
+            panic!("{e}");
+        }
+    };
+
+    let mut parser = Parser::new(source, result);
+
+    let ast_result = match parser.parse() {
+        Ok(ast) => ast,
+        Err(msg) => {
+            panic!("{msg}");
+        }
+    };
+
+    let result = Interpreter::new(ast_result).interpret().unwrap();
+
+    result
+}
+
+/// Check that the interpreter works for if statements.
+#[test]
+fn test_interpret_if() {
+    let result = get_program_result("if x <= 5 then x := 1 else x := 0");
+    let mut expected_result = State::new();
+    expected_result.set("x".to_string(), 1);
+    assert_eq!(result, expected_result)
+}
