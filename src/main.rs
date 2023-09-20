433ded7fc6449bb0e290759554691d30fbf6b0d8
TwistedTornado <144802073+TwistedTornado@users.noreply.github.com>
2023-09-20 14:19:20 +0100

    Rewrite main.rs to read from file and add interpret stage

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use std::fs::File;
+use std::io::Read;
 use std::process::exit;
+use while_tools::interpreter::Interpreter;
 
 use while_tools::lexer::Lexer;
 use while_tools::parser::Parser;
@@ -6,7 +9,38 @@ use while_tools::parser::Parser;
 fn main() {
     // This is a basic example showing the pipeline as to how the source is
     // processed.
-    let source = "x := 5; while x <= 100 do x := x + 1";
+
+    // Getting a handle on the file.
+    println!("Opening file...");
+
+    let file_path = "examples/gcd.while";
+
+    let mut file = match File::open(file_path) {
+        Ok(f) => {
+            println!("File opened successfully.");
+            f
+        }
+        Err(e) => {
+            println!("{e}");
+            exit(0);
+        }
+    };
+
+    // Reading the file into a string, assuming valid UTF-8
+
+    let mut source_string = String::new();
+
+    println!("\nReading file...");
+
+    match file.read_to_string(&mut source_string) {
+        Ok(b) => {
+            println!("File read successfully ({b} bytes).")
+        }
+        Err(e) => {
+            println!("Failed reading `{file_path}` to string: {e}");
+            exit(0)
+        }
+    }
 
     // First is the lexing. `Lexer` itself is an `Iterator`, so we can collect
     // on it directly. But we need to check for, and handle, `LexError`s.
@@ -19,9 +53,15 @@ fn main() {
     // iterators, we'd have to change this later on).
     //
     // In short, we need fallible iterators.
-    let tokens = Lexer::new(source.chars());
+    println!("\nLexing...");
+
+    let tokens = Lexer::new(source_string.chars());
+
     let result: Vec<_> = match tokens.collect::<Result<Vec<_>, _>>() {
-        Ok(v) => v,
+        Ok(v) => {
+            println!("Lexed successfully ({} tokens).", v.len());
+            v
+        }
         Err(e) => {
             println!("{e}");
             exit(0);
@@ -31,7 +71,8 @@ fn main() {
     // Now that we have an errorless stream of tokens, we parse. The parser needs
     // access to the source so that it can extract identifier names and literal
     // values and add them into the AST.
-    let mut parser = Parser::new(source, result);
+    println!("\nParsing...");
+    let mut parser = Parser::new(&source_string, result);
 
     let ast = match parser.parse() {
         Ok(ast) => ast,
@@ -41,9 +82,16 @@ fn main() {
         }
     };
 
-    // If we didn't error, we can print out the AST. As of now, it's span-less.
-    dbg!(ast);
+    println!("Parsed.");
 
-    // This is as far as is currently implemented. The next step is to interpret
-    // this AST.
+    // The next step is to interpret this AST.
+
+    println!("\nInterpreting...");
+
+    let result = Interpreter::new(ast).interpret();
+
+    match result {
+        Ok(state) => println!("Interpreted. {:?}", state),
+        Err(e) => println!("{e}"),
+    }
 }
