dbd38a66314e3f298a3d554345b6a006a48392bc
TwistedTornado <144802073+TwistedTornado@users.noreply.github.com>
2023-09-21 22:47:50 +0100

    Add SourceNavigator methods to get position and line content

--- src/source_navigator.rs
+++ src/source_navigator.rs
@@ -25,6 +25,35 @@ impl<'a> SourceNavigator<'a> {
 
         Self { source, line_heads }
     }
+
+    /// Gives the line and column of a particular index.
+    pub fn get_position(&self, index: usize) -> FilePos2d {
+        let (this_line_index, (this_start, _)) = self
+            .line_heads
+            .iter()
+            .zip(self.line_heads.iter().skip(1))
+            .enumerate()
+            .find(|(_, (_, next_head))| !(**next_head < index))
+            .unwrap();
+
+        FilePos2d {
+            row: this_line_index,
+            col: index - this_start,
+        }
+    }
+
+    /// Given a line number, returns that line in the source stream.
+    /// Trims newlines.
+    pub fn get_line(&self, line_index: usize) -> &'a str {
+        let this_line_start = self.line_heads[line_index];
+        let next_line_start = self
+            .line_heads
+            .get(line_index + 1)
+            .unwrap_or(&self.source.len())
+            .to_owned();
+
+        &self.source[this_line_start..next_line_start - 1].trim()
+    }
 }
 
 #[derive(Copy, Clone, Debug)]
